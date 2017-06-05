bd993abaaf4020f5421dcb108c439e3a51e20e7c
Yang Wang <deepcontrolr@gmail.com>
2017-06-05 04:18:41 -0700

    All done

--- Burp/RecipeDetailViewController.swift
+++ Burp/RecipeDetailViewController.swift
@@ -155,14 +155,15 @@ class RecipeDetailViewController: UIViewController, UITableViewDelegate, UITable
 	}
 	
 
-    /*
+	
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+		if segue.identifier == "detailToComment" {
+			let dest = segue.destination as! commentTableViewController
+			dest.id = id
+		}
     }
-    */
-
+	
 }
